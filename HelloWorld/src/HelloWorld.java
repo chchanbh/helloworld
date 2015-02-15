8d2b482f0435e78ae81571a0b308f27114484322
chchanbh <chchanbh@stu.ust.hk>
2015-02-15 14:35:17 +0800

    This is for lab assignment #2

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World! remote change local change");
+		System.out.println("Hello World. Comp3021, Lab1 ");
+		// I have successfully cloned my projects, and finished my assignent
+		//Now I want to push my code to the remote repository
 	}
 
 }
